0b4c284b402b059607460b29567080a6542730ec
shura08g <shura08g@rambler.ru>
2021-02-09 14:07:01 +0200

    Add files via upload

---
+++ TIC-TAC-TOE/src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            player_turn = "X",
+            board: ["", "", "", "", "", "", "", "", ""]
+        }
+    }
+
+    squareClicked(index) {
+        let player_turn = this.state.player_turn
+        let board = this.state.board
+        console.log("index", index)
+        board[index] = player_turn
+
+        let winning_combos = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for (let i = 0; i < winning_combos.length; i++) {
+            let winning_row = winning_combos[i]
+            let p1 = winning_combos[0]
+            let p2 = winning_combos[1]
+            let p3 = winning_combos[2]
+
+            if (board[p1] != "" && board[p1] == board[p2] && board[p2] == board[p3] && board[p3] == board[p1]) {
+                alert("Winner! player ${player_turn} has won the game!")
+            }
+        }
+
+        player_turn = (player_turn == "X") ? "0" : "X"
+
+        this.SetState({
+            player_turn: player_turn,
+            board: board
+        })
+
+
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <h1>Tic tac toe</h1>
+                <div className="board">
+                    {this.state.board.map((square) =>{
+                        return (<div onClick={() => this.squareClicked(index)} className="square"><h3 className="symbol">(square)</h3></div>)
+                    })}
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
